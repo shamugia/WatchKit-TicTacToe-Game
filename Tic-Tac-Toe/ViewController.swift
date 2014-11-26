d0c61edd280a2ffbb012eb1b4fc0004cfd513e5f
George Shamugia <George@Georges-MacBook-Pro.local>
2014-11-26 15:30:13 +0400

    Initial Commit

---
+++ Tic-Tac-Toe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tic-Tac-Toe
+//
+//  Created by George Shamugia on 26/11/2014.
+//  Copyright (c) 2014 George Shamugia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
